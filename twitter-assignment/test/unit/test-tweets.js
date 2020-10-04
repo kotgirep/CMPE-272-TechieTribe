259a3ff242679207a32b394c53b8dc4963a4c42a
pranjali kotgire <pranjalikotgire57@gmail.com>
2020-10-03 20:18:25 -0700

    unit-test to test the re-tweet method

--- twitter-assignment/test/unit/test-tweets.js
+++ twitter-assignment/test/unit/test-tweets.js
@@ -4,6 +4,7 @@ let chai = require("chai");
 let should = chai.should();
 let expect = chai.expect;
 let server = require("../../app");
+let tweet_id_created = null;
 
 describe("CreateTweetTest", function () {
   it("should return 200 if create tweet is successful", (done) => {
@@ -14,6 +15,18 @@ describe("CreateTweetTest", function () {
       .request(server)
       .post("/tweets/create/")
       .send(tweet_this)
+      .end((err, res) => {
+        res.should.have.status(200);
+        tweet_id_created = res.body["id_str"];
+        done();
+      });
+  });
+
+  it("should return 200 if re-tweet is successful", (done) => {
+    let ts = new Date().getTime();
+    chai
+      .request(server)
+      .post("/tweets/" + tweet_id_created)
       .end((err, res) => {
         res.should.have.status(200);
         done();
