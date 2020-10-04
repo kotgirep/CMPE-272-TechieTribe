e0d97748fc9027c0e4dfa2a3df4d2e1c68338987
supriyameduri9 <supriya.meduri@sjsu.edu>
2020-10-03 18:20:53 -0700

    adding search and like functionality

--- twitter-assignment/routes/index.js
+++ twitter-assignment/routes/index.js
@@ -1,13 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var request = require("request");
+var Twitter = require("twitter");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+require('dotenv').config();
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Express hello' });
-});
+/* Search tweet associated with the given 'search word' and 'count' - returns the no of tweets based on count
+ req content-type: JSON
+Author: Supriya Meduri */
 
+
+function searchTweet(req, res, next) {
+
+    var searchKey = req.body.search_message
+    var countNo = req.body.search_count;
+    let params = {
+        q: searchKey,
+        count: countNo
+    };
+
+    client.get('search/tweets', params, function (error, tweet, response) {
+        if (error) {
+            console.log("Unable to find the tweet with search word and count ! Error:" + JSON.stringify(error));
+            res.status(404).json({
+                error: 'Unable to find the tweet !'
+            });
+        } else {
+            console.log('Tweet(s) found !')
+            res.status(200).json(tweet)
+        }
+
+        return next();
+    });
+
+};
+
+router.get('/search', searchTweet);
 module.exports = router;
+module.exports.searchTweet = searchTweet;
+
--- twitter-assignment/routes/tweets.js
+++ twitter-assignment/routes/tweets.js
@@ -5,58 +5,87 @@ var Twitter = require("twitter");
 
 require("dotenv").config();
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("this is tweets end-point !!");
+    res.send("this is tweets end-point !!");
 });
 
 /*[POST] To create a tweet or update status */
 // req content-type: JSON
 router.post("/create/", function (req, res, next) {
-  var status_message = req.body.tweet_message;
-  console.log("trying to print request body: ");
-  console.log(req.body);
-  client.post("statuses/update", { status: status_message }, function (
-    error,
-    tweet,
-    response
-  ) {
-    if (error) {
-      console.log("Unable to create tweet! Error: " + JSON.stringify(error));
-      return res.status(500).json({
-        error: "Unable to post tweet!",
-      });
-    }
-    console.log("Successfully posted tweet!");
-    console.log(tweet.id_str);
-    //console.log(tweet); // Tweet body.
-    res.status(200).json(tweet);
-  });
+    var status_message = req.body.tweet_message;
+    console.log("trying to print request body: ");
+    console.log(req.body);
+    client.post("statuses/update", {status: status_message}, function (
+        error,
+        tweet,
+        response
+    ) {
+        if (error) {
+            console.log("Unable to create tweet! Error: " + JSON.stringify(error));
+            return res.status(500).json({
+                error: "Unable to post tweet!",
+            });
+        }
+        console.log("Successfully posted tweet!");
+        console.log(tweet.id_str);
+        //console.log(tweet); // Tweet body.
+        res.status(200).json(tweet);
+    });
 });
 
 /*[POST] To do a re-tweet */
 router.post("/:tweetId", function (req, res, next) {
-  var tweetId = req.params["tweetId"];
-  console.log("tweet ID of tweet to re-tweet: ");
-  console.log(tweetId);
+    var tweetId = req.params["tweetId"];
+    console.log("tweet ID of tweet to re-tweet: ");
+    console.log(tweetId);
 
-  client.post("statuses/retweet/" + tweetId, function (error, tweet, response) {
-    if (error) {
-      console.log("Unable to re-tweet! Error: " + JSON.stringify(error));
-      return res.status(500).json({
-        error: error,
-      });
-    }
-    console.log("Successfully re-tweeted !");
-    console.log(tweet.text);
-    res.status(200).json(tweet);
-  });
+    client.post("statuses/retweet/" + tweetId, function (error, tweet, response) {
+        if (error) {
+            console.log("Unable to re-tweet! Error: " + JSON.stringify(error));
+            return res.status(500).json({
+                error: error,
+            });
+        }
+        console.log("Successfully re-tweeted !");
+        console.log(tweet.text);
+        res.status(200).json(tweet);
+    });
 });
+
+
+/*[POST] To like a tweet
+  Author: Supriya Meduri
+ */
+router.post("/like/:tweetId", function (req, res) {
+    var tweetId = req.params["tweetId"];
+    console.log("tweet ID of tweet to favourite ");
+    console.log(tweetId);
+
+    client.post("favorites/create", {id: tweetId}, function (
+        error,
+        tweet,
+        response
+    ) {
+        if (error) {
+            console.log("Unable to like the tweet! Error:" + JSON.stringify(error));
+            error: 'error';
+
+        };
+
+        console.log("Successfully liked the tweet ! ")
+
+        console.log(tweet.text);
+        res.status(200).json(tweet);
+
+    });
+})
+
 
 module.exports = router;
