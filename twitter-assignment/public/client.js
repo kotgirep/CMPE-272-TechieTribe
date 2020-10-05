7b027246ac9e0b555fed0b954c53fbeecef4a7e0
pranjali kotgire <pranjalikotgire57@gmail.com>
2020-10-04 22:48:38 -0700

    re-tweet UI component and backend support for the same

--- twitter-assignment/public/client.js
+++ twitter-assignment/public/client.js
@@ -38,46 +38,49 @@ form[0].addEventListener('submit', (event) => {
     loadingElement.style.display = '';
 
     const tweet = {
-      tweet_message:content
+      tweet_message: content,
     };
-    
+
     fetch('http://localhost:3000/tweets/create', {
       method: 'POST',
       mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
       body: JSON.stringify(tweet),
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => {      
-      if (!response.ok) {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('json')) {
-          return response.json().then(error => Promise.reject(error.message));
-        } else {
-          return response.text().then(message => Promise.reject(message));
+        'content-type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          const contentType = response.headers.get('content-type');
+          if (contentType.includes('json')) {
+            return response
+              .json()
+              .then((error) => Promise.reject(error.message));
+          } else {
+            return response.text().then((message) => Promise.reject(message));
+          }
         }
-      }
-    }).then(() => {
-      form[0].reset();
-      setTimeout(() => {
+      })
+      .then(() => {
+        form[0].reset();
+        setTimeout(() => {
+          form[0].style.display = '';
+        }, 300);
+        listAllTweets();
+      })
+      .catch((errorMessage) => {
         form[0].style.display = '';
-      }, 300);
-      listAllTweets();
-    }).catch(errorMessage => {
-      form[0].style.display = '';
-      errorElement[0].textContent = errorMessage;
-      errorElement[0].style.display = '';
-      loadingElement.style.display = 'none';
-    });
+        errorElement[0].textContent = errorMessage;
+        errorElement[0].style.display = '';
+        loadingElement.style.display = 'none';
+      });
   } else {
     errorElement[0].textContent = 'Name and content are required!';
     errorElement[0].style.display = '';
   }
 });
-
-
 
 //This to manipulate data from form 1: Search Tweets
 form[1].addEventListener('submit', (event) => {
@@ -91,17 +94,17 @@ form[1].addEventListener('submit', (event) => {
     form[1].style.display = 'none';
     loadingElement.style.display = '';
 
-      form[1].reset();
-      setTimeout(() => {
-        form[1].style.display = '';
-      }, 300);
-      searchAllTweets(noOfTweets, searchString);
+    form[1].reset();
+    setTimeout(() => {
+      form[1].style.display = '';
+    }, 300);
+    searchAllTweets(noOfTweets, searchString);
   } else {
-    errorElement[1].textContent = 'No of Tweets and Search Message are required!';
+    errorElement[1].textContent =
+      'No of Tweets and Search Message are required!';
     errorElement[1].style.display = '';
   }
 });
-
 
 //This to manipulate data from form 3: Delete Tweets
 form[2].addEventListener('submit', (event) => {
@@ -115,39 +118,44 @@ form[2].addEventListener('submit', (event) => {
     loadingElement.style.display = '';
 
     const tweetDelete = {
-      tweetid:tweetID
+      tweetid: tweetID,
     };
-    
+
     fetch('http://localhost:3000/tweets/destroy/', {
       method: 'DELETE',
       mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
       body: JSON.stringify(tweetDelete),
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => {      
-      if (!response.ok) {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('json')) {
-          return response.json().then(error => Promise.reject(error.message));
-        } else {
-          return response.text().then(message => Promise.reject(message));
+        'content-type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          const contentType = response.headers.get('content-type');
+          if (contentType.includes('json')) {
+            return response
+              .json()
+              .then((error) => Promise.reject(error.message));
+          } else {
+            return response.text().then((message) => Promise.reject(message));
+          }
         }
-      }
-    }).then(() => {
-      form[2].reset();
-      setTimeout(() => {
+      })
+      .then(() => {
+        form[2].reset();
+        setTimeout(() => {
+          form[2].style.display = '';
+        }, 300);
+        listAllTweets();
+      })
+      .catch((errorMessage) => {
         form[2].style.display = '';
-      }, 300);
-      listAllTweets();
-    }).catch(errorMessage => {
-      form[2].style.display = '';
-      errorElement[2].textContent = errorMessage;
-      errorElement[2].style.display = '';
-      loadingElement.style.display = 'none';
-    });
+        errorElement[2].textContent = errorMessage;
+        errorElement[2].style.display = '';
+        loadingElement.style.display = 'none';
+      });
   } else {
     errorElement[2].textContent = 'Name and content are required!';
     errorElement[2].style.display = '';
@@ -163,39 +171,84 @@ function deleteTweet(tweetID) {
     loadingElement.style.display = '';
 
     const tweetDelete = {
-      tweetid:tweetID
+      tweetid: tweetID,
     };
-    
+
     fetch('http://localhost:3000/tweets/destroy/', {
       method: 'DELETE',
       mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
       body: JSON.stringify(tweetDelete),
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => {      
-      if (!response.ok) {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('json')) {
-          return response.json().then(error => Promise.reject(error.message));
-        } else {
-          return response.text().then(message => Promise.reject(message));
+        'content-type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          const contentType = response.headers.get('content-type');
+          if (contentType.includes('json')) {
+            return response
+              .json()
+              .then((error) => Promise.reject(error.message));
+          } else {
+            return response.text().then((message) => Promise.reject(message));
+          }
         }
-      }
-    }).then(() => {
-      listAllTweets();
-    }).catch(errorMessage => {
-      errorElement[2].textContent = errorMessage;
-      errorElement[2].style.display = '';
-      loadingElement.style.display = 'none';
-    });
+      })
+      .then(() => {
+        listAllTweets();
+      })
+      .catch((errorMessage) => {
+        errorElement[2].textContent = errorMessage;
+        errorElement[2].style.display = '';
+        loadingElement.style.display = 'none';
+      });
   } else {
     errorElement[2].textContent = 'Name and content are required!';
     errorElement[2].style.display = '';
   }
-};
+}
+
+function reTweet(tweetID) {
+  if (tweetID.trim()) {
+    loadingElement.style.display = '';
+    const reTweetURL = 'http://localhost:3000/tweets/' + tweetID;
+
+    fetch(reTweetURL, {
+      method: 'POST',
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          const contentType = response.headers.get('content-type');
+          if (contentType.includes('json')) {
+            return response
+              .json()
+              .then((error) => Promise.reject(error.message));
+          } else {
+            return response.text().then((message) => Promise.reject(message));
+          }
+        }
+      })
+      .then(() => {
+        listAllTweets();
+      })
+      .catch((errorMessage) => {
+        errorElement[2].textContent = errorMessage;
+        errorElement[2].style.display = '';
+        loadingElement.style.display = 'none';
+      });
+  } else {
+    errorElement[2].textContent = 'Name and content are required!';
+    errorElement[2].style.display = '';
+  }
+}
 
 function loadMore() {
   skip += limit;
@@ -211,24 +264,25 @@ function listAllTweets(reset = true) {
   }
 
   fetch('http://localhost:3000/index/list')
-  .then(response => response.json())
-    .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       console.log(result.statuses);
       console.log(result);
-      result.forEach(status => {
+      result.forEach((status) => {
         const div = document.createElement('div');
 
         const header = document.createElement('h6');
         header.textContent = status.user.name;
-        
-	const header1 = document.createElement('small');
+
+        const header1 = document.createElement('small');
         header1.textContent = status.user.screen_name;
 
         const contents = document.createElement('p');
         contents.textContent = status.text;
 
         const date = document.createElement('small');
-        date.textContent = new Date(status.created_at) +' ID: ' + status.id_str;
+        date.textContent =
+          new Date(status.created_at) + ' ID: ' + status.id_str;
 
         div.appendChild(header);
         //div.appendChild(header1);
@@ -262,17 +316,17 @@ function listAllTweets(reset = true) {
   }
 
   fetch('http://localhost:3000/index/list')
-  .then(response => response.json())
-    .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       console.log(result.statuses);
       console.log(result);
-      result.forEach(status => {
+      result.forEach((status) => {
         const div = document.createElement('div');
 
         const header = document.createElement('h5');
         header.textContent = status.user.name;
-        
-	      const header1 = document.createElement('small');
+
+        const header1 = document.createElement('small');
         header1.textContent = status.user.screen_name;
 
         const contents = document.createElement('p');
@@ -281,21 +335,37 @@ function listAllTweets(reset = true) {
         const date = document.createElement('small');
         date.textContent = new Date(status.created_at);
 
-        var btn = document.createElement("button");
-        btn.style = "float: right; border:none;";
-        btn.setAttribute("tweed-id", status.id_str);
-        btn.addEventListener("click", function() {
-          deleteTweet(this.getAttribute("tweed-id"));
+        var btn = document.createElement('button');
+        btn.style = 'float: right; border:none;';
+        btn.setAttribute('tweed-id', status.id_str);
+        btn.addEventListener('click', function () {
+          deleteTweet(this.getAttribute('tweed-id'));
         });
 
-        var iconspan = document.createElement("span");
-        iconspan.style.color = "#00B7FF";
-        iconspan.style.fontSize = "20px";
-        iconspan.setAttribute("class", "glyphicon glyphicon-trash");
+        var btn2 = document.createElement('button');
+        btn2.style = 'float: right; border:none;';
+        btn2.setAttribute('tweed-id', status.id_str);
+        btn2.addEventListener('click', function () {
+          reTweet(this.getAttribute('tweed-id'));
+        });
+
+        var iconspan = document.createElement('span');
+        iconspan.style.color = '#00B7FF';
+        iconspan.style.fontSize = '20px';
+        iconspan.setAttribute('class', 'glyphicon glyphicon-trash');
+
         btn.appendChild(iconspan);
+
+        var iconspan2 = document.createElement('span');
+        iconspan2.style.color = '#00B7FF';
+        iconspan2.style.fontSize = '20px';
+        iconspan2.setAttribute('class', 'glyphicon glyphicon-retweet');
+
+        btn2.appendChild(iconspan2);
 
         div.appendChild(header);
         div.appendChild(btn);
+        div.appendChild(btn2);
         div.appendChild(contents);
         div.appendChild(date);
 
@@ -313,7 +383,6 @@ function listAllTweets(reset = true) {
     });
 }
 
-
 function searchAllTweets(noOfTweets, searchString, reset = true) {
   loading = true;
   if (reset) {
@@ -321,25 +390,26 @@ function searchAllTweets(noOfTweets, searchString, reset = true) {
     skip = 0;
     finished = false;
   }
-    const searchJSON = {
-      search_message: searchString,
-      search_count: noOfTweets
-    };
-   
-    fetch('http://localhost:3000/index/search', {
-      method: 'POST',
-      mode: 'cors', // no-cors, *cors, same-origin
+  const searchJSON = {
+    search_message: searchString,
+    search_count: noOfTweets,
+  };
+
+  fetch('http://localhost:3000/index/search', {
+    method: 'POST',
+    mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
-      body: JSON.stringify(searchJSON),
-      headers: {
-        'content-type': 'application/json'
-      }
-  }).then(response => response.json())
-    .then(result => {
+    body: JSON.stringify(searchJSON),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
       console.log(result.statuses);
       console.log(result);
-      result.statuses.forEach(status => {
+      result.statuses.forEach((status) => {
         const div = document.createElement('div');
 
         const header = document.createElement('h5');
@@ -349,7 +419,7 @@ function searchAllTweets(noOfTweets, searchString, reset = true) {
         contents.textContent = status.text;
 
         const date = document.createElement('small');
-        date.textContent = new Date(status.created_at) +' ID: ' + status.id;
+        date.textContent = new Date(status.created_at) + ' ID: ' + status.id;
 
         div.appendChild(header);
         div.appendChild(contents);
